7fd42b87a4b7557f35c4a28f25408615b481b8a8
Mike Harms <mike.harms@ovative.com>
2018-08-17 15:24:08 -0500

    First Commit

---
+++ code/codeGitProject.R
@@ -0,0 +1,14 @@
+##########################################
+## Analysis Script for Project A
+##   2017-09-01
+##
+##  Script takes in "A_processed_data.csv"
+##  Creates Tables 1-4, Figures 1, 2 for submission to
+##    Journal of Cool Results
+#########################################
+setwd("~/gitRProject")
+
+# Start Script ------------------------------------------------------------
+
+#take a random sample twice
+sample(1:100, size=10)
